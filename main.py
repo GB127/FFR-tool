8f0d56b62f142822421ff98cfee3a258105235e6
Guylain <guylain.breton.1@ulaval.ca>
2020-05-05 23:38:05 -0400

    Create a new  .py for future plans
    and simplify stuffs

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from classes import FFRclasses,\
-    Fighter, Knight,\
-    Thief, Ninja,\
-    BlackBelt, Master,\
-    RedMage, RedWizard,\
-    WhiteMage, WhiteWizard,\
-    BlackMage, BlackWizard
+from classes import *
 from Equips import weapon, listweapons, armor, listarmors  # this works
-
+from affichage import *
 import os
 
 
@@ -20,6 +14,18 @@ acrodicto = {"FI" : Fi, "KN" : Kn, "BB" : BB, "MA" : Ma, "TH" : Th, "NI" : Ni,
 
 
 
+def print_table(what, liste):
+    print(" " * 24 + "| A  | E  |" + f"{char1.acro:^3}|{char2.acro:^3}|{char3.acro:^3}|{char4.acro:^3}|")
+    print("-" * 50)
+    listcan = []
+    for item in liste :
+        if any([i.checkequip(what, item.cat, item.name) for i in listchar]):
+            listcan.append(item)
+    listcan.sort()
+    for i in listcan:
+        line = str(i) + f"{char1.checkequip(what, i.cat, i.name, True):^3}|"  + f"{char2.checkequip(what, i.cat, i.name, True):^3}|"  + f"{char3.checkequip(what, i.cat, i.name, True):^3}|"  + f"{char4.checkequip(what, i.cat, i.name, True):^3}|"
+        print(line)
+    print("-" * 50)
 
 
 if __name__ == "__main__":
@@ -31,39 +37,17 @@ if __name__ == "__main__":
     char4 = acrodicto[input("What is your 4th class? ").upper()]()
     listchar = [char1, char2, char3, char4]
 
+
+
     gaming = True
     while gaming :
         print("END = End the game, LA = List Armor, LW = List Weapons")
         command = input("What would you like to do? ").upper()
+        clear()
         if command == "LA":
-            clear()
-            print(" " * 24 + "| A  | E  |" + f"{char1.acro:^3}|{char2.acro:^3}|{char3.acro:^3}|{char4.acro:^3}|")
-            print("-" * 50)
-            listcan = []
-            for armor in listarmors :
-                if any([i.checkequip("Armory", armor.cat, armor.name) for i in listchar]):
-                    listcan.append(armor)
-            listcan.sort()
-            for i in listcan:
-                line = str(i) + f"{char1.checkequip('Armory', i.cat, i.name, True):^3}|"  + f"{char2.checkequip('Armory', i.cat, i.name, True):^3}|"  + f"{char3.checkequip('Armory', i.cat, i.name, True):^3}|"  + f"{char4.checkequip('Armory', i.cat, i.name, True):^3}|"
-                print(line)
-            print("-" * 50)
+            print_table("Armory", listarmors)
         elif command == "LW":
-            clear()
-            print(" " * 24 + "| D  | H  |" + f"{char1.acro:^3}|{char2.acro:^3}|{char3.acro:^3}|{char4.acro:^3}|")
-            print("-" * 50)
-
-            listcan = []
-            for weapon in listweapons :
-                if any([i.checkequip("Weapons", weapon.cat, weapon.name) for i in listchar]):
-                    listcan.append(weapon)
-            listcan.sort()
-            for i in listcan:
-                line = str(i) + f"{char1.checkequip('Weapons', i.cat, i.name, True):^3}|"  + f"{char2.checkequip('Weapons', i.cat, i.name, True):^3}|"  + f"{char3.checkequip('Weapons', i.cat, i.name, True):^3}|"  + f"{char4.checkequip('Weapons', i.cat, i.name, True):^3}|"
-                print(line)
-
+            print_table("Weapons", listweapons)
         elif command == "END":
+            print("Script Stopped")
             gaming = None
-
-
-
