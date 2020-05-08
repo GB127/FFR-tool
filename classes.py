43e8002bd24287d2c817ad691d2477fe7ec8abe4
Guylain <guylain.breton.1@ulaval.ca>
2020-05-08 12:55:19 -0400

    Update Fighter

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+from Equips import weapon, armor
+
 class FFRError(BaseException):
     pass  # We'll see
 class FFRclasses:
@@ -28,22 +30,70 @@ class FFRclasses:
 class Fighter(FFRclasses):
     def __init__(self):
         self.acro = "Fi"
-        self.canequip = {
-                        "Armory" : {
-                                    "Armors" : ["Cloth", "Wooden", "Chain", "Iron", "Silver", "Steel", "Ice", "Flame"],
-                                    "Bracelets": ["Copper", "Silver", "Gold", "Opal"],
-                                    "Shields" : ["Wooden", "Iron", "Silver", "Buckler", "Flame", "Ice", "ProCape"],
-                                    "Helmets" : ["Cap", "Wooden", "Iron", "Silver", "Ribbon"],
-                                    "Gauntlets" : ["Gloves", "Copper", "Iron", "Silver","Power", "ProRing"]
-                                    },
-                        "Weapons": {"Swords" : ["Short","Were", "Rune", "Dragon", "Coral", "Long", "Silver", "Giant", "Flame", "Ice", "Sun"],
-                                    "Axes" : ["Hand", "Great", "Silver", "Light"],
-                                    "Daggers" : ["Small", "Large", "Silver"],
-                                    "Staffs" : ["Wooden","Power", "Iron"],
-                                    "Hammers" : ["Iron", "Silver"],
-                                    "Nunchucks" : [],
-                                    "Others" : ["Rapier", "Scimtar", "Sabre", "Falchon", "Masmune"]},
-                        }
+        self.canequip = [
+            armor("Armors","Cloth",1,-2),
+            armor("Armors","Wooden",4,-5),
+            armor("Armors","Chain",15,-15),
+            armor("Armors","Iron",24,-23),
+            armor("Armors","Silver",18,-8),
+            armor("Armors","Steel",34,-33),
+            armor("Armors","Ice",34,-10),
+            armor("Armors","Flame",34,-10),    
+            armor("Bracelets","Copper",4,-1),
+            armor("Bracelets","Silver",15,-1),
+            armor("Bracelets","Gold",24,-1),
+            armor("Bracelets","Opal",36,-1),
+            armor("Shields","Wooden",2,0),
+            armor("Shields","Iron",4,0),
+            armor("Shields","Silver",8,0),
+            armor("Shields","Buckler",2,0),
+            armor("Shields","Flame",12,0),
+            armor("Shields","Ice",12,0),
+            armor("Shields","ProCape",8,-2),
+            armor("Helmets","Cap",1,-1),
+            armor("Helmets","Wooden",3,-3),
+            armor("Helmets","Iron",5,-5),
+            armor("Helmets","Silver",6,-3),
+            armor("Helmets","Ribbon",1,-1),
+            armor("Gauntlets","Gloves",1,-1),
+            armor("Gauntlets","Copper",2,-3),
+            armor("Gauntlets","Iron",4,-5),
+            armor("Gauntlets","Silver",6,-3),
+            armor("Gauntlets","Power",6,-3),
+            armor("Gauntlets","ProRing",8,-1),
+            weapon("Swords","Short",15,10),
+            weapon("Swords","Rune",18,15),
+            weapon("Swords","Were",18,15),
+            weapon("Swords","Dragon",19,15),
+            weapon("Swords","Coral",19,15),
+            weapon("Swords","Long",20,10),
+            weapon("Swords","Silver",23,15),
+            weapon("Swords","Giant",21,20),
+            weapon("Swords","Flame",26,20),
+            weapon("Swords","Ice",29,25),
+            weapon("Swords","Sun",32,30),
+            weapon("Axes","Hand",16,5),
+            weapon("Axes","Great",22,5),
+            weapon("Axes","Silver",25,10),
+            weapon("Axes","Light",28,15),
+
+            weapon("Daggers","Small",5,10),
+            weapon("Daggers","Large",7,10),
+            weapon("Daggers","Silver",10,15),
+
+            weapon("Staffs","Wooden",6, 0),
+            weapon("Staffs","Power",12,0),
+            weapon("Staffs","Iron",14,0),
+
+            weapon("Hammers","Iron",9,0),
+            weapon("Hammers","Silver",12,5),
+
+
+            weapon("Others","Rapier",9,5),
+            weapon("Others","Scimtar",10,10),
+            weapon("Others","Sabre",13,5),
+            weapon("Others","Falchon",15,10),
+            weapon("Others","Masmune",56,50)]
 class Knight(Fighter):
     def __init__(self):
         super().__init__()
