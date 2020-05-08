33cf220b2b019c8d868f983120d9a9e9317915fc
Guylain <guylain.breton.1@ulaval.ca>
2020-05-07 21:45:53 -0400

    Move a function to elsewhere so it's easier to maintain

--- main.py
+++ main.py
@@ -15,25 +15,6 @@ acrodicto = {"FI" : Fi, "KN" : Kn, "BB" : BB, "MA" : Ma, "TH" : Th, "NI" : Ni,
 
 
 
-def print_table(what, liste, listechar):
-    """Produces a string that represent the whole list along with
-        the characteristics of the classes in the team. Then print it.
-        Arguments:
-            what (str) = "Armory" or "Weapons"
-            liste (list) = uniform list of weapon or armor objects
-            listechar = list of FFRclasses in the team
-    """
-    string = " " * 27 + "| A  | E  |"
-    for char in listechar:
-        string += f"{char.acro:^3}|"
-    string += "\n" + ("-" * 38) + "----" * len(listechar)
-    for no,i in enumerate(liste):
-        line = f'{str(no):<2}  ' + str(i)
-        for char in listechar:
-            line += f"{char.checkequip(what, i.cat, i.name, True):^3}|"
-        string += f"\n{line}"
-    string += "\n" + ("-" * 38) + "----" * len(listechar)
-    print(string)
 
 
 if __name__ == "__main__":
